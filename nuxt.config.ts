3b15005d0d86bf114a585d2a186be94318a7a051
Bern Vein Balermo <realbernvein@gmail.com>
2024-11-07 15:22:00 +0800

    initialization

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    '@nuxt/icon',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    //'@nuxtjs/supabase'
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-red-400">
+        Test
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
